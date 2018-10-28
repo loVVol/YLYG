ca7f8991db8325d0dfcbe6febea674a8c3bc4691
IoVVoI <549365883@qq.com>
2018-10-29 06:37:03 +0800

    完成主角移动初级代码未处理

--- app/src/main/java/com/jbf/ylyg/MODEL/FightSurfaceView.java
+++ app/src/main/java/com/jbf/ylyg/MODEL/FightSurfaceView.java
@@ -10,6 +10,9 @@ import android.text.method.*;
 
 public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callback,Runnable
 {
+
+ 	/**每30帧刷新一次屏幕**/
+	public static final int TIME_IN_FRAME = 30;
 	private long mAutoPlayCount=0;
 	private boolean mIsAutoPlay=true;
 	private int mScreenWidth=0;
@@ -22,28 +25,90 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 	private boolean mIsALive=true;
 	private boolean mIfLeftButtonDown;
 	private boolean mIfRightButtonDown;
-	
-	private Point mLeftButtonPointCenter;
-	private Point mRightButtonPointCenter;
-	private Point mAutoPlayButtonPointCenter;
-	private Point mSmallMapPointCenter;
+
+	private PointF mLeftButtonPointCenter;
+	private PointF mLeftButtonPointSmallCenter;
+	private PointF mRightButtonPointCenter;
+	private PointF mAutoPlayButtonPointCenter;
+	private PointF mSmallMapPointCenter;
 	private Path mAutoPlayButtonPath;
 	private Path mAutoPlayPath;
 	private Path mNotAutoPlayPath;
 	private Path mSmallMapPath;
 	private int mLeftButtonSize;
+	private int mLeftButtonSmallSize;
 	private int mRightButtonSize;
 	private int mSmallMapSize;
 	private int mAutoPlayButtonSize;
+	private Player mPlayer;
 	Canvas mCanvas;
+	Canvas mplayerCanvas;
 
-
+	public String testa=null;
+	public String testb=null;
 
 
 	public FightSurfaceView(Context context)
 	{
 		super(context);
 		initEverything();
+	}
+	protected void initEverything()
+	{
+		setFocusable(true);
+		setKeepScreenOn(true);
+		mSurfaceHolder = this.getHolder();
+		mSurfaceHolder.addCallback(this);
+		mCanvas = new Canvas();
+		mRandom = new Random(System.currentTimeMillis());
+		DisplayMetrics dm=new DisplayMetrics();
+		((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(dm);
+		mScreenWidth = dm.widthPixels;
+		mScreenHeight = dm.heightPixels;
+
+		mPaint = new Paint();
+		mAutoPlayPath = new Path();
+		mNotAutoPlayPath = new Path();
+		mSmallMapPath = new Path();
+		mAutoPlayButtonPath = new Path();
+		mSmallMapPointCenter = new PointF();
+		mAutoPlayButtonPointCenter = new PointF();
+		mLeftButtonPointCenter = new PointF();
+		mRightButtonPointCenter = new PointF();
+		mLeftButtonPointSmallCenter = new PointF();
+		int x,y;
+		x = mScreenWidth;
+		y = mScreenHeight;
+		if (x < y)
+		{
+			int i=0;
+			i = x;
+			x = y;
+			y = i;
+		}
+		mLeftButtonSize = x * 3 / 32;
+		mLeftButtonSmallSize = mLeftButtonSize / 3;
+		mRightButtonSize = x * 3 / 32;
+		mAutoPlayButtonSize = x / 64;
+		mSmallMapSize = x / 16;
+		mLeftButtonPointCenter.set(x * 3 / 100 + mLeftButtonSize, y - x * 2 / 100 - y / 100 - mLeftButtonSize);
+		mLeftButtonPointSmallCenter.set(x * 3 / 100 + mLeftButtonSize, y - x * 2 / 100 - y / 100 - mLeftButtonSize);
+		mRightButtonPointCenter.set(x * 97 / 100 - mRightButtonSize, y - x * 2 / 100 - y / 100 - mRightButtonSize);
+		mAutoPlayButtonPointCenter.set(x * 97 / 100 - mAutoPlayButtonSize, y / 2);
+		mSmallMapPointCenter.set(x * 98 / 100 - mSmallMapSize, x / 100 + y / 100 + mSmallMapSize);
+
+		mAutoPlayButtonPath.addCircle(mAutoPlayButtonPointCenter.x, mAutoPlayButtonPointCenter.y, mAutoPlayButtonSize, Path.Direction.CW);
+
+		mSmallMapPath.addRect(mSmallMapPointCenter.x - mSmallMapSize,
+							  mSmallMapPointCenter.y - mSmallMapSize,
+							  mSmallMapPointCenter.x + mSmallMapSize,
+							  mSmallMapPointCenter.y + mSmallMapSize,
+							  Path.Direction.CW);
+		mAutoPlayPath.addRect(x / 3, y / 100, x * 99 / 100, y * 99 / 100, Path.Direction.CW);
+		mNotAutoPlayPath.addRect(x / 100, y / 100, x * 99 / 100, y * 99 / 100 , Path.Direction.CW);
+		mPlayer = new Player();
+
+
 	}
 
 	@Override
@@ -71,73 +136,92 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 	{
 		while (true)
 		{
-			AI();
-			render();
+
+
+			/**取得更新之前的时间**/
+			long startTime = System.currentTimeMillis();
+
+			/**在这里加上线程安全锁**/
+			synchronized (mSurfaceHolder)
+			{
+
+				AI();
+				render();
+			}
+			/**取得更新结束的时间**/
+			long endTime = System.currentTimeMillis();
+
+			/**计算出一次更新的毫秒数**/
+			int diffTime  = (int)(endTime - startTime);
+
+			/**确保每次更新时间为30帧**/
+			while (diffTime <= 1000 / TIME_IN_FRAME)
+			{
+				diffTime = (int)(System.currentTimeMillis() - startTime);
+				/**线程等待**/
+				Thread.yield();
+
+			}
+			// TODO: Implement this method
 		}
-		// TODO: Implement this method
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event)
 	{
 		// TODO: Implement this method
-		return super.onTouchEvent(event);
-	}
+		//boolean mLeftButtonDown;
 
-	protected void initEverything()
-	{
-		setFocusable(true);
-		setKeepScreenOn(true);
-		mSurfaceHolder = this.getHolder();
-		mSurfaceHolder.addCallback(this);
-		mCanvas = new Canvas();
-		mRandom = new Random(System.currentTimeMillis());
-		DisplayMetrics dm=new DisplayMetrics();
-		((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(dm);
-		mScreenWidth = dm.widthPixels;
-		mScreenHeight = dm.heightPixels;
-
-		mPaint = new Paint();
-		mAutoPlayPath = new Path();
-		mNotAutoPlayPath = new Path();
-		mSmallMapPath = new Path();
-		mAutoPlayButtonPath = new Path();
-		mSmallMapPointCenter = new Point();
-		mAutoPlayButtonPointCenter = new Point();
-		mLeftButtonPointCenter = new Point();
-		mRightButtonPointCenter = new Point();
-		int x,y;
-		x = mScreenWidth;
-		y = mScreenHeight;
-		if (x < y)
+		float xf=event.getX();
+		float yf=event.getY();
+		switch (event.getAction())
 		{
-			int i=0;
-			i = x;
-			x = y;
-			y = i;
+			case MotionEvent.ACTION_DOWN:
+				{
+					if (Math.pow((xf - mLeftButtonPointCenter.x), 2)
+						+ Math.pow((yf - mLeftButtonPointCenter.y), 2)
+						<= Math.pow((mLeftButtonSize * 2), 2))
+					{
+						mIfLeftButtonDown = true;
+						setLeftButtonDown(xf, yf);
+
+					}
+					break;
+
+				}
+			case MotionEvent.ACTION_POINTER_DOWN:
+				{
+
+				}
+
+			case MotionEvent.ACTION_MOVE:
+				{
+					if (mIfLeftButtonDown)
+					{
+						setLeftButtonDown(xf, yf);
+					}
+					break;
+				}
+			case MotionEvent.ACTION_POINTER_UP:
+				{
+
+				}
+			case MotionEvent.ACTION_UP:
+				{
+					mIfLeftButtonDown = false;
+					mLeftButtonPointSmallCenter.set(mLeftButtonPointCenter.x, mLeftButtonPointCenter.y);
+					break;
+				}
+			case MotionEvent.ACTION_CANCEL:
+				{
+
+				}
 		}
-		mLeftButtonSize = x * 3 / 32;
-		mRightButtonSize = x * 3 / 32;
-		mAutoPlayButtonSize = x / 64;
-		mSmallMapSize = x / 16;
-		mLeftButtonPointCenter.set(x * 3 / 100 + mLeftButtonSize, y - x * 2 / 100 - y / 100 - mLeftButtonSize);
-		mRightButtonPointCenter.set(x * 97 / 100 - mRightButtonSize, y - x * 2 / 100 - y / 100 - mRightButtonSize);
-		mAutoPlayButtonPointCenter.set(x * 97 / 100 - mAutoPlayButtonSize, y / 2);
-		mSmallMapPointCenter.set(x * 98 / 100 - mSmallMapSize, x / 100 + y / 100 + mSmallMapSize);
 		
-		mAutoPlayButtonPath.addCircle(mAutoPlayButtonPointCenter.x, mAutoPlayButtonPointCenter.y, mAutoPlayButtonSize, Path.Direction.CW);
-		
-		mSmallMapPath.addRect(mSmallMapPointCenter.x - mSmallMapSize,
-							  mSmallMapPointCenter.y - mSmallMapSize,
-							  mSmallMapPointCenter.x + mSmallMapSize,
-							  mSmallMapPointCenter.y + mSmallMapSize,
-							  Path.Direction.CW);
-		mAutoPlayPath.addRect(x / 3, y / 100, x * 99 / 100, y * 99 / 100, Path.Direction.CW);
-		mNotAutoPlayPath.addRect(x / 100, y / 100, x *99/ 100, y *99/ 100 , Path.Direction.CW);
-
-
-
+		return true;
 	}
+
+
 	private void drawInit()
 	{
 
@@ -154,7 +238,7 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 		{
 
 			mCanvas.drawPath(mAutoPlayPath, mPaint);
-			
+
 
 		}
 		else
@@ -164,11 +248,20 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 			mCanvas.drawCircle(mLeftButtonPointCenter.x, mLeftButtonPointCenter.y, mLeftButtonSize, mPaint);
 			mCanvas.drawCircle(mRightButtonPointCenter.x, mRightButtonPointCenter.y, mRightButtonSize, mPaint);
 
+			mCanvas.drawCircle(mLeftButtonPointSmallCenter.x, mLeftButtonPointSmallCenter.y, mLeftButtonSmallSize, mPaint);
+			mPaint.setTextSize(30);
+			if (testa != null && testb != null)
+			{
+
+
+				mCanvas.drawText(testa, 50, 50, mPaint);
+				mCanvas.drawText(testb, 50, 80, mPaint);
+			}
 
 		}
 		mCanvas.drawPath(mSmallMapPath, mPaint);
 		mCanvas.drawPath(mAutoPlayButtonPath, mPaint);
-
+		mPlayer.draw(mCanvas);
 
 
 	}
@@ -180,7 +273,7 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 		{
 			if (mCanvas != null)
 			{
-				mCanvas.save();
+				//mCanvas.save();
 				drawInit();
 				drawUI();
 				if (mGameIsContinue)
@@ -206,8 +299,71 @@ public class FightSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 		}
 	}
 	private void AI()
-	{}
+	{
+		if(mIfLeftButtonDown){
+			double a= (mLeftButtonPointSmallCenter.x - mLeftButtonPointCenter.x) / mLeftButtonSize;
+			double b=(mLeftButtonPointSmallCenter.y - mLeftButtonPointCenter.y) / mLeftButtonSize;
+			double c=Math.atan(mLeftButtonPointSmallCenter.y / mLeftButtonPointSmallCenter.x);
+			mPlayer.setMove(a, b, c);
+			//testa = String.valueOf(xf);
+			//testb = String.valueOf(yf);
+			}
+	}
 
+	private void setLeftButtonDown(float xf, float yf)
+	{
+		if (Math.pow((xf - mLeftButtonPointCenter.x), 2)
+			+ Math.pow((yf - mLeftButtonPointCenter.y), 2)
+			< Math.pow((mLeftButtonSize), 2))
+		{
+			mLeftButtonPointSmallCenter.set((int)xf, (int)yf);
+
+		}
+		else
+		{
+			if (xf == mLeftButtonPointCenter.x)
+			{
+				if (yf > mLeftButtonPointCenter.y)
+				{
+					mLeftButtonPointSmallCenter.set(mLeftButtonPointCenter.x, mLeftButtonPointCenter.y + mLeftButtonSize);
+				}
+				else
+				{
+					mLeftButtonPointSmallCenter.set(mLeftButtonPointCenter.x, mLeftButtonPointCenter.y - mLeftButtonSize);
+				}
+			}
+			else if (yf == mLeftButtonPointCenter.y)
+			{
+				if (xf > mLeftButtonPointCenter.x)
+				{
+					mLeftButtonPointSmallCenter.set(mLeftButtonPointCenter.x + mLeftButtonSize, mLeftButtonPointCenter.y);
+				}
+				else
+				{
+					mLeftButtonPointSmallCenter.set(mLeftButtonPointCenter.x - mLeftButtonSize, mLeftButtonPointCenter.y);
+				}
+			}
+			else 
+			{
+				double ddx=Math.sqrt(Math.pow(mLeftButtonSize, 2) / (1 + Math.pow((yf - mLeftButtonPointCenter.y) / (xf - mLeftButtonPointCenter.x), 2)));
+				double x=0;
+				if (xf > mLeftButtonPointCenter.x)
+				{
+					x = mLeftButtonPointCenter.x + ddx;
+				}
+				else
+				{
+					x = mLeftButtonPointCenter.x - ddx;
+				}
+				double y=mLeftButtonPointCenter.y + (yf - mLeftButtonPointCenter.y) * (x - mLeftButtonPointCenter.x) / (xf - mLeftButtonPointCenter.x);
+				mLeftButtonPointSmallCenter.set((int)x, (int)y);
+			}
+
+			
+		}
+		double mmm=Math.sqrt(Math.pow((mLeftButtonPointSmallCenter.x - mLeftButtonPointCenter.x), 2) + Math.pow((mLeftButtonPointSmallCenter.y - mLeftButtonPointCenter.y), 2)) / mLeftButtonSize;
+		
+	}
 
 
 }
--- app/src/main/java/com/jbf/ylyg/MODEL/Player.java
+++ app/src/main/java/com/jbf/ylyg/MODEL/Player.java
@@ -1,6 +1,74 @@
 package com.jbf.ylyg.MODEL;
+import android.graphics.*;
 
 public class Player
 {
-	
+	private Canvas mCanvas;
+	private PointF mMapPoint;
+	private PointF mScreenPoint;
+	private Paint mPaint;
+	private float mRotate;
+	private int mSpeed=150;
+	public Player()
+	{
+		
+		this.mRotate=0;
+		this.mPaint=new Paint();
+		this.mMapPoint=new PointF();
+		this.mScreenPoint=new PointF();
+		//
+		this.mScreenPoint.set(500,500);//测试用
+		//
+	}
+
+	public void setMMapPoint(PointF mMapPoint)
+	{
+		this.mMapPoint = mMapPoint;
+	}
+
+	public PointF getMMapPoint()
+	{
+		return mMapPoint;
+	}
+
+	public void setMScreenPoint(PointF mScreenPoint)
+	{
+		
+		this.mScreenPoint = mScreenPoint;
+	}
+
+	public PointF getMScreenPoint()
+	{
+		return mScreenPoint;
+	}
+	public void setMPain(Paint mPaint)
+	{
+		this.mPaint=mPaint;
+	}
+	public void setMRotate(int mRotate)
+	{
+		this.mRotate=mRotate;
+	}
+	public void draw(Canvas canvas)
+	{
+		mCanvas=canvas;
+		mPaint.setTextSize(70);
+		mPaint.setColor(Color.WHITE);
+		
+		//mCanvas.save();
+		//mCanvas.rotate(mScreenPoint.x,mScreenPoint.y,mRotate);
+		mCanvas.drawText("我是player",mScreenPoint.x,mScreenPoint.y,mPaint);
+		//mCanvas.restore();
+		
+	}
+	public void setMove(double x,double y,double rotate)
+	{
+		this.mRotate=(int)rotate;
+		this.mMapPoint.x+=x*mSpeed/30;
+		this.mMapPoint.y+=y*mSpeed/30;
+		this.mScreenPoint.x=(float)x*mSpeed/30+this.mScreenPoint.x;
+		this.mScreenPoint.y=(float)y*mSpeed/30+this.mScreenPoint.y;
+		
+		
+	}
 }
